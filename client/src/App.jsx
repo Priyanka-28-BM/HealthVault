49ca01072cb5b25248db1613719d9af49251d6ff
khushi <khushishukla2323@gmail.com>
2025-07-26 16:01:21 +0530

    fixed bugs of multiple Router, supabaseKey and userProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Navbar2 from './components/Navbar2';
-import Footer from './components/Footer';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Home from './components/Home';
-import MedicalFiles from './components/medicalfiles';
-import ForgotPassword from './components/ForgotPassword';
-import ResetPassword from './components/ResetPassword';
-import UpdatePassword from './Pages/UpdatePassword';
-import AuthRedirect from './Pages/AuthRedirect';
+import Navbar2 from "./components/Navbar2";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import MedicalFiles from "./components/medicalfiles";
+import ForgotPassword from "./components/ForgotPassword";
+import ResetPassword from "./components/ResetPassword";
+import UpdatePassword from "./Pages/UpdatePassword";
+import AuthRedirect from "./Pages/AuthRedirect";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar2 />
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -27,13 +27,13 @@ function App() {
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/update-password" element={<UpdatePassword />} />
         <Route path="/auth-redirect" element={<AuthRedirect />} />
-        
+
         {/* Optional 404 Page */}
         {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
 
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
